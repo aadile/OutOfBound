a9ad2e055485ca68b2c3bb6e692792e4dec110ff
zohra051 <senouci12@hotmail.fr>
2020-05-06 10:00:36 +0200

    custom ui

--- lib/zht/pages/StartPositionPage.dart
+++ lib/zht/pages/StartPositionPage.dart
@@ -1,4 +1,5 @@
 import 'package:OutOfBounds/zht/LocationProvider.dart';
+
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:location/location.dart';
@@ -8,10 +9,31 @@ class StartPosition extends StatefulWidget {
   @override
   StartPositionState createState() => StartPositionState();
 }
+class MyClipper extends CustomClipper<Path> {
+
+  @override
+  Path getClip(Size size) {
+  var path = Path();
+  path.lineTo(0, size.height-50);
+  path.quadraticBezierTo(
+    size.width / 2, 
+    size.height,
+    size.width,
+    size.height - 50
+  );
+  path.lineTo(size.width, 0.0);
+    path.close();
+    return path;
+  }
+
+  @override
+  bool shouldReclip(MyClipper oldClipper) => false;
+}
 
 class StartPositionState extends State<StartPosition> {
   Geolocator geolocator = new Geolocator();
   double distanceInMeters;
+
 
   Future<double> distanceBetween2(
       LocationData current, LocationData start) async {
@@ -32,6 +54,7 @@ class StartPositionState extends State<StartPosition> {
     });
   }
 
+
   @override
   Widget build(BuildContext context) {
     return Consumer<LocationProvider>(builder: (context, _provider, _) {
@@ -39,22 +62,45 @@ class StartPositionState extends State<StartPosition> {
         updateDistance(_provider);
         return Scaffold(
             backgroundColor: Colors.blue,
-            body: Center(
-                child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
+            body: Column(
+               children: <Widget>[
                   Text(
                       "LATITUDE: ${_provider.starting.latitude}, LONGITUDE: ${_provider.starting.longitude}"),
                   Text("Actually at $distanceInMeters meters from this point.")
-                ])));
+               ]));
       } else {
         return Scaffold(
             backgroundColor: Colors.blue,
-            body: Center(
-              child: Text(
-                  "You don't have a start position ! \n Please choose a Start Position"),
-            ));
+            body: Column( 
+               children: <Widget>[
+                 Stack( 
+              children: <Widget>[
+                Container(             
+                  decoration: BoxDecoration(color: Colors.red),
+                  child: ClipPath(
+                    clipper: MyClipper(), 
+                    child:Image.asset('assets/images/googlemap.png'),     
+                  ),
+                ),
+                Positioned(
+                  left:100.0,
+                  top:100.0,
+                  child:ClipOval(
+                    child:Container(
+                      height: 180,
+                      decoration: BoxDecoration(color:Colors.black),
+                      child: Image.asset('assets/images/gear.png', height:6.0),
+                    )
+                  ), 
+                ),                 
+                Text("You don't have a start position ! \n Please choose a Start Position"),
+              ]
+                 )
+               ]
+            )
+          );
+        }
       }
-    });
+    );
   }
 }
